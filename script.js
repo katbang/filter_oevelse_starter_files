222e3b988303cd4e8bb5d1a79175e71ab56704d7
katbang <katr164m@stud.kea.dk>
2023-08-29 17:41:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,143 @@
+const vehicles = [
+  {
+    type: "Bus",
+    fuel: "Diesel",
+    passengers: 45,
+    stops: ["Nørrebrogade", "Elmegade"],
+  },
+  { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
+  {
+    type: "Cykel",
+    fuel: "Rugbrød",
+    passengers: 0,
+    ownedBy: "Jonas",
+    isElectric: true,
+  },
+  { type: "Bil", passengers: 5, ownedBy: "Elon", isElectric: true },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Fonda" },
+  {
+    type: "Cykel",
+    fuel: "Rugbrød",
+    passengers: 2,
+    ownedBy: "Vingegård",
+    isTandem: true,
+  },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Yolanda" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Børge" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
+  { type: "Løbehjul", passengers: 1, isElectric: true },
+];
+
+const ulPointer = document.querySelector("ul");
+const newInner = `<li><strong>Type</strong></li>
+<li><strong>Fuel</strong></li>
+<li><strong>Passengers</strong></li>
+<li><strong>Stops</strong></li>
+<li><strong>OwnedBy</strong></li>
+<li><strong>Electric</strong></li>
+<li><strong>Tandem</strong></li>`;
+
+showTheseVehicles(vehicles);
+
+function showAll() {
+  ulPointer.innerHTML = "";
+  ulPointer.innerHTML = newInner;
+  showTheseVehicles(vehicles);
+}
+
+function showTheseVehicles(arr) {
+  arr.forEach((each) => {
+    ulPointer.innerHTML += `<li>${each.type}</li>`;
+    if (each.fuel === undefined) {
+      ulPointer.innerHTML += `<li>-</li>`;
+    } else {
+      ulPointer.innerHTML += `<li>${each.fuel}</li>`;
+    }
+    ulPointer.innerHTML += `<li>${each.passengers}</li>`;
+    if (each.stops === undefined) {
+      ulPointer.innerHTML += `<li>-</li>`;
+    } else {
+      ulPointer.innerHTML += `<li>${each.stops}</li>`;
+    }
+    if (each.ownedBy === undefined) {
+      ulPointer.innerHTML += `<li>-</li>`;
+    } else {
+      ulPointer.innerHTML += `<li>${each.ownedBy}</li>`;
+    }
+    if (each.isElectric === undefined) {
+      ulPointer.innerHTML += `<li>-</li>`;
+    } else {
+      ulPointer.innerHTML += `<li>X</li>`;
+    }
+    if (each.isTandem === undefined) {
+      ulPointer.innerHTML += `<li>-</li>`;
+    } else {
+      ulPointer.innerHTML += `<li>X</li>`;
+    }
+  });
+}
+
+function isElectric(vehicle) {
+  if (vehicle.isElectric === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function showElectrics() {
+  ulPointer.innerHTML = "";
+  ulPointer.innerHTML = newInner;
+  const onlyElectric = vehicles.filter(isElectric);
+  showTheseVehicles(onlyElectric);
+}
+document.querySelector(".el").addEventListener("mousedown", showElectrics);
+
+function minTwoSeats(vehicle) {
+  if (vehicle.passengers >= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function showMinTwo() {
+  ulPointer.innerHTML = "";
+  ulPointer.innerHTML = newInner;
+  const moreThanOneSeat = vehicles.filter(minTwoSeats);
+  showTheseVehicles(moreThanOneSeat);
+}
+
+document.querySelector(".two").addEventListener("mousedown", showMinTwo);
+
+function electricJonas(vehicle) {
+  if (vehicle.isElectric === true && vehicle.ownedBy === "Jonas") {
+    return true;
+  } else {
+    return false;
+  }
+}
+function showJonasEl() {
+  ulPointer.innerHTML = "";
+  ulPointer.innerHTML = newInner;
+  const onlyElectricJonas = vehicles.filter(electricJonas);
+  showTheseVehicles(onlyElectricJonas);
+}
+document.querySelector(".jonas").addEventListener("mousedown", showJonasEl);
+
+function twoSeatedRugbrød(vehicle) {
+  if (vehicle.fuel === "Rugbrød" && vehicle.passengers > 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function showTwoRugbrød() {
+  ulPointer.innerHTML = "";
+  ulPointer.innerHTML = newInner;
+  const minTwoSeatedRugbrød = vehicles.filter(twoSeatedRugbrød);
+  showTheseVehicles(minTwoSeatedRugbrød);
+}
+document
+  .querySelector(".rugbrød")
+  .addEventListener("mousedown", showTwoRugbrød);
+
+document.querySelector(".alle").addEventListener("mousedown", showAll);
